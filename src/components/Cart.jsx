124cf7a4fdeb4eed48d55f0d073297440e809101
Tamás Major <majortamas313@gmail.com>
2024-05-08 11:43:31 +0200

    checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -17,8 +17,12 @@ export default function Cart() {
         userProgressCtx.hideCart();
     }
 
+    function handleGoToCheckout() {
+        userProgressCtx.showCheckout();
+    }
+
     return (
-        <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+        <Modal className="cart" open={userProgressCtx.progress === 'cart'} onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}>
             <h2>Your cart</h2>
             <ul>
                 {cartCtx.items.map((item) =>
@@ -35,7 +39,7 @@ export default function Cart() {
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
                 <Button textOnly onClick={handleCloseCart}>Close</Button>
-                <Button>Go to Checkout</Button>
+                {cartCtx.items.length > 0 && (<Button onClick={handleGoToCheckout}>Go to Checkout</Button>)}
             </p>
         </Modal>
     )
