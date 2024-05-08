2a1431ad607126100c6e17fc0e4bf1abdeef8769
Tamás Major <majortamas313@gmail.com>
2024-05-08 15:10:18 +0200

    error handling

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from "./MealItem.jsx";
 import useHttp from "./hooks/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -7,7 +8,11 @@ export default function Meals() {
     const { data: loadedMeals, error, isLoading } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
     if (isLoading) {
-        return <p>Fetching meals...</p>
+        return <p className="center">Fetching meals...</p>
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />
     }
 
     return (
